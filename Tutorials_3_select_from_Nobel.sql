15b924c2340e037ef475d2db50217f8a406f38dd
MrSayyod <ssayyod@gmail.com>
2020-05-31 21:31:12 +0500

    Deletes indentetion

--- Tutorials_3_select_from_Nobel.sql
+++ Tutorials_3_select_from_Nobel.sql
@@ -96,5 +96,4 @@ ORDER BY yr DESC
 SELECT winner, subject 
   FROM nobel
  WHERE yr=1984
- ORDER BY subject IN ('Physics','Chemistry'), subject, winner
- 
+ ORDER BY subject IN('Physics', 'Chemistry'), subject, winner
